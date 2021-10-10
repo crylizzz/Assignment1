d797a7a054bc5cdd746fa49dd9229297e5b0c10b
crylizzz <anzym@protonmail.com>
2021-10-10 18:51:01 +0200

    Initial commit

---
+++ LoginTroels/Models/ListOfAdults.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace LoginTroels.Models
+{
+    public class ListOfAdults
+    {
+        public List<Adult> Adults { get; set; }
+    }
+}
